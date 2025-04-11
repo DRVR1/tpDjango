cbbd0a0c26bf12fec1f40ab9385ff4959bf9d3b7
DRVR1 <ianvidmar200@gmail.com>
2025-04-11 11:10:56 -0300

    [Parte 3.2] Urls y html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from .models import Question
 from django.template import loader
 from django.shortcuts import render
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -14,10 +15,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
@@ -28,3 +26,5 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
